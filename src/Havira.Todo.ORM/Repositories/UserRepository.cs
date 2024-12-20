fe1071601e2a4b3a693488b5715857a7ae8f09a4
Guilherme Otsu <guiotsu@gmail.com>
2024-12-20 16:18:13 -0300

    feat: implemented UserRepository

---
+++ src/Havira.Todo.ORM/Repositories/UserRepository.cs
@@ -0,0 +1,53 @@
+using Havira.Todo.Domain.Entities;
+using Havira.Todo.Domain.Repostories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Havira.Todo.ORM.Repositories;
+
+/// <summary>
+/// Implementation of IUserRepository using Entity Framework Core
+/// </summary>
+public class UserRepository : IUserRepository
+{
+    private readonly DefaultContext _context;
+
+    public UserRepository(DefaultContext context)
+    {
+        _context = context;
+    }
+    
+    /// <summary>
+    /// Creates a new user in the database
+    /// </summary>
+    /// <param name="user">The user to create</param>
+    /// <param name="cancellationToken">Cancellation token</param>
+    /// <returns>The created user</returns>
+    public async Task<User> CreateAsync(User user, CancellationToken cancellationToken = default)
+    {
+        await _context.Users.AddAsync(user, cancellationToken);
+        await _context.SaveChangesAsync(cancellationToken);
+        return user;
+    }
+    
+    /// <summary>
+    /// Retrieves a user by their unique identifier
+    /// </summary>
+    /// <param name="id">The unique identifier of the user</param>
+    /// <param name="cancellationToken">Cancellation token</param>
+    /// <returns>The user if found, null otherwise</returns>
+    public async Task<User?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        return await _context.Users.FirstOrDefaultAsync(o=> o.Id == id, cancellationToken);
+    }
+
+    /// <summary>
+    /// Retrieves a user by their email
+    /// </summary>
+    /// <param name="email">The email of the user</param>
+    /// <param name="cancellationToken">Cancellation token</param>
+    /// <returns>The user if found, null otherwise</returns>
+    public async Task<User?> GetByEmailAsync(string email, CancellationToken cancellationToken = default)
+    {
+        return await _context.Users.FirstOrDefaultAsync(o => o.Email == email, cancellationToken);
+    }
+}
